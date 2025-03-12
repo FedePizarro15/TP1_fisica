f5d4fa0ff300d731c5d43d459a2c0ac1db87f802
mathikneipp <mkneipp@udesa.edu.ar>
2025-03-12 18:38:24 -0300

    Correccion de escala log y ajuste lineal

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def diametros_(archivo = "Trabajo Práctico 1.xlsx"):
 
     return np.array(np.concatenate((datos1, datos2, datos3)))
 
-def std_(array):
+def std_(array: np.array):
     return np.std(array, ddof=1)
 
 def calcular_error(desviacion):
@@ -26,12 +26,22 @@ def masas_(archivo = "Trabajo Práctico 1.xlsx"):
     
     return np.array(np.concatenate((datos1, datos2, datos3)))
 
-def graficar_promedios(diametros, masas, errores, escala_log: bool):
+def graficar_promedios(diametros: np.array, masas: np.array, errores: float, escala_log: bool, ajuste_lineal: bool):
+    masas = np.array(masas, dtype=float)
+    diametros = np.array(diametros, dtype=float)
     if escala_log:
-        plt.xscale("log")
-        plt.yscale("log")
+        masas = np.log(masas)
+        diametros = np.log(diametros)
+    
     plt.scatter(masas, diametros, color="darkblue", label="Datos", s=60, alpha=0.8)
     plt.errorbar(masas, diametros, yerr=errores, fmt="o", color="c", label="Desviación estándar")
+    
+    if ajuste_lineal:
+        # Ajuste lineal mediante mínimos cuadrados
+        m, b = np.polyfit(masas, diametros, 1)  # grado 1 para línea
+        y_fit = m * masas + b
+        plt.plot(masas, y_fit, color="red", label=f'Ajuste lineal: y = {m:.2f}x + {b:.2f}')
+        
     plt.xlabel("Masas")
     plt.ylabel("Diámetros")
     plt.title("Masa en función del diámetro")
@@ -40,7 +50,7 @@ def graficar_promedios(diametros, masas, errores, escala_log: bool):
     plt.show()
     
 def main():
-    graficar_promedios(diametros_(), masas_(), calcular_error(std_(diametros_())), True)
+    graficar_promedios(diametros_(), masas_(), calcular_error(std_(diametros_())), True, True)
     
 if __name__ == "__main__":
     main()
