f89254877491c36305ee80cd0843056efe821416
mathikneipp <mkneipp@udesa.edu.ar>
2025-03-13 13:11:49 -0300

    arreglo de la desviacion estandar

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def diametros_(archivo = "Trabajo Práctico 1.xlsx"):
+def diametros_(archivo = "Trabajo Práctico 1.xlsx") -> np.array:
     df = pd.read_excel(archivo, engine="openpyxl")
     datos1 = df.iloc[3:13, 4:7].mean(axis=1).values
     datos2 = df.iloc[15:19, 4:7].mean(axis=1).values
@@ -10,15 +10,18 @@ def diametros_(archivo = "Trabajo Práctico 1.xlsx"):
 
     return np.array(np.concatenate((datos1, datos2, datos3)))
 
-def std_(array: np.array):
-    return np.std(array, ddof=1)
+def std_(array: np.array) -> np.array:
+    desviacion_std = []
+    for i in array:
+        desviacion_std.append(np.std(np.array([i, i+0.5, i-0.5])))
+    return np.array(desviacion_std)
 
 def calcular_error(desviacion):
     error_instrumental = 0.1 # (0.1 mm)
     error_total = np.sqrt(desviacion**2 + error_instrumental**2)
     return error_total
 
-def masas_(archivo = "Trabajo Práctico 1.xlsx"):
+def masas_(archivo = "Trabajo Práctico 1.xlsx") -> np.array:
     df = pd.read_excel(archivo, engine="openpyxl")
     datos1 = df.iloc[3:13, 10].values
     datos2 = df.iloc[15:19, 10].values
@@ -26,19 +29,20 @@ def masas_(archivo = "Trabajo Práctico 1.xlsx"):
     
     return np.array(np.concatenate((datos1, datos2, datos3)))
 
-def graficar_promedios(diametros: np.array, masas: np.array, errores: float, escala_log: bool, ajuste_lineal: bool):
+def graficar_promedios(diametros: np.array, masas: np.array, desviacion_std: np.array, escala_log: bool, ajuste_lineal: bool):
+    print(np.shape(desviacion_std))
     masas = np.array(masas, dtype=float)
     diametros = np.array(diametros, dtype=float)
+    desviacion_std = np.array(desviacion_std, dtype=float)
     if escala_log:
         masas = np.log(masas)
         diametros = np.log(diametros)
+        desviacion_std = 0 # A chequear
     
-    plt.scatter(masas, diametros, color="darkblue", label="Datos", s=60, alpha=0.8)
-    plt.errorbar(masas, diametros, yerr=errores, fmt="o", color="c", label="Desviación estándar")
+    plt.errorbar(masas, diametros, yerr=desviacion_std, fmt=".", color="c", label="Desviación estándar")
     
     if ajuste_lineal:
-        # Ajuste lineal mediante mínimos cuadrados
-        m, b = np.polyfit(masas, diametros, 1)  # grado 1 para línea
+        m, b = np.polyfit(masas, diametros, 1)
         y_fit = m * masas + b
         plt.plot(masas, y_fit, color="red", label=f'Ajuste lineal: y = {m:.2f}x + {b:.2f}')
         
@@ -50,7 +54,7 @@ def graficar_promedios(diametros: np.array, masas: np.array, errores: float, esc
     plt.show()
     
 def main():
-    graficar_promedios(diametros_(), masas_(), calcular_error(std_(diametros_())), True, True)
+    graficar_promedios(diametros_(), masas_(), std_(diametros_()), escala_log=False, ajuste_lineal=False)
     
 if __name__ == "__main__":
     main()
